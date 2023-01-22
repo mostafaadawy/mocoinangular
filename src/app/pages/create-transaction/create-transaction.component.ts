92cd9e91f7d35488e0f830e395436c4d0872af89
mostafaadawy <mostafa_adawy@ymail.com>
2023-01-22 22:37:28 +0200

    create transaction

--- src/app/pages/create-transaction/create-transaction.component.ts
+++ src/app/pages/create-transaction/create-transaction.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { BlockchainService } from 'src/app/services/blockchain.service';
+import { Transaction } from '../../blockchain/blockchain'
 @Component({
   selector: 'app-create-transaction',
   templateUrl: './create-transaction.component.html',
   styleUrls: ['./create-transaction.component.scss']
 })
-export class CreateTransactionComponent {
+export class CreateTransactionComponent implements OnInit{
+  public newTx;
+  public walletKey;
+  constructor(private blockchainService:BlockchainService) {
+    this.walletKey= blockchainService.walletKeys[0];
+  }
+  ngOnInit(){
+    this.newTx=new Transaction();
+  }
+  createTransaction(){
+    this.newTx.fromAddress=this.walletKey.public
+    this.newTx.signTransaction(this.walletKey.keyObj)
+
+    this.blockchainService.addTransaction(this.newTx)
+    this.newTx= new Transaction()
+  }
 
 }
