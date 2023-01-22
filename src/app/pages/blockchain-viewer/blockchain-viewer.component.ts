a076eafd6ce46f61e2b5e579c03c7f7d52bf8591
mostafaadawy <mostafa_adawy@ymail.com>
2023-01-22 16:55:14 +0200

    sync to video tutorial

--- src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
+++ src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
@@ -8,36 +8,10 @@ import { BlockchainService } from '../../services/blockchain.service';
 export class BlockchainViewerComponent implements OnInit {
 
   public blocks = [];
-  public selectedBlock = null;
-
   constructor(private blockchainService: BlockchainService) {
     this.blocks = blockchainService.blockchainInstance.chain;
-    this.selectedBlock = this.blocks[0];
-    console.log(this.blocks);
   }
 
   ngOnInit() {
-  }
-
-  showTransactions(block) {
-    console.log(block);
-    this.selectedBlock = block;
-    return false;
-  }
-
-  blockHasTx(block) {
-    return block.transactions.length > 0;
-  }
-
-  selectedBlockHasTx() {
-    return this.blockHasTx(this.selectedBlock);
-  }
-
-  isSelectedBlock(block) {
-    return this.selectedBlock === block;
-  }
-
-  getBlockNumber(block) {
-    return this.blocks.indexOf(block) + 1;
   }
 }
